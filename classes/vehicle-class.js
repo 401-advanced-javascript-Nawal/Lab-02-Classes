17f5b3ef038262ba0fb93ffd885d7d93198e628c
Nsuliman <nsuliman88@gmail.com>
2020-01-22 04:01:19 +0200

    Vehicle constructor Done

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -1,6 +1,35 @@
 'use strict';
 
-class Car {
-}
+class Vehicle {
+
+    constructor(name, wheels) {
+
+        this.name = name;
+        this.wheels = wheels;
+    }
+
+    drive()
+    {
+        return 'Moving Forward';
+    }
+
+    stop()
+    {
+        return 'Stopping';
+    }
+} // end of Vehicle Class 
+
+
+class Car extends Vehicle {
+
+    constructor(name, wheels) {
+
+        // To be able to use the Vehicle constructor we use SUPER either for methods 
+        super(name,4);
+    }
+
+    super.drive()
+
+} // end of Car Class 
 
 module.exports = Car;
