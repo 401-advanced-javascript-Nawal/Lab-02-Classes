b441955a4cf9a84ab782ea44423819701493a6d4
Nsuliman <nsuliman88@gmail.com>
2020-01-22 04:22:07 +0200

    Index.js Done

--- classes/index.js
+++ classes/index.js
@@ -3,11 +3,21 @@
 const VehicleConstructor = require('./vehicle-constructor.js');
 
 // Implement a car and motorcycle using a Constructor
+
+console.log(' \n\n****************** Machines Constructors *******************');
 const mazda = new VehicleConstructor.Car('Mazda 3');
 console.log(mazda.name, mazda.drive(), mazda.stop());
 
 const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
 
-// Implement a car and motorcycle using a Class
 
+// Implement a car and motorcycle using a Class
+const VehicleClass = require('./vehicle-class.js')
+
+console.log(' \n\n ****************** Machines Classes ************************* ');
+const mazdaClass = new VehicleClass.Car('Mazda 3');
+console.log('Mazda Name :' , mazdaClass.name , '\n Mazda Drive() :' , mazdaClass.drive() , '\n Mazda Stop() :' , mazdaClass.stop());
+
+const harleyClass = new VehicleClass.Motorcycle('Harley');
+console.log('\n Harley Name :' , harleyClass.name , '\n Harley wheelie() :' , harleyClass.wheelie() , '\n Harley Stop() :' , harleyClass.stop());
